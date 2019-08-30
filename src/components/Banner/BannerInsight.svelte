22ed00c453a6bffe8abe42c6b077693a51555f1f
DmitriVanGuard <dimavanguard@gmail.com>
2019-08-30 09:41:01 +0300

    Sticky banner

--- src/components/Banner/BannerInsight.svelte
+++ src/components/Banner/BannerInsight.svelte
@@ -14,6 +14,10 @@
   function toggleVisibility() {
     isShown = !isShown
   }
+
+  function onSubmit(e) {
+    console.log(e)
+  }
 </script>
 
 <template lang="pug">
@@ -24,16 +28,19 @@ ViewportObserver({options}, on:intersect='{toggleVisibility}', on:leaving='{togg
 
 +if('isShown')
   +panel.banner
-    .img
+    img(src='sticky_banner_img.png')
     .content
       .text
-        h2.title Aloha
-        p.desc test
-      .form
-        subscribe
+        h2.title Want more crypto insights?
+        p.desc Read daily analysis of top emerging words/stories
+      form.form(on:submit|preventDefault='{onSubmit}')
+        +input(type='email', placeholder='Enter your email')
+        +button.submit(variant='fill', accent='jungle-green', type='submit') Get started
 </template>
 
 <style lang="scss">
+  @import '@/mixins';
+
   .banner {
     height: 90px;
     box-shadow: 0px 10px 20px rgba(24, 27, 43, 0.1);
@@ -45,5 +52,49 @@ ViewportObserver({options}, on:intersect='{toggleVisibility}', on:leaving='{togg
     border-radius: 10px;
     z-index: 10;
     max-width: 85%;
+    padding: 3px 18px;
+    display: flex;
+  }
+
+  img {
+    width: 93px;
+    margin-right: 19px;
+  }
+
+  .content {
+    display: flex;
+    align-items: center;
+  }
+
+  .content,
+  .text,
+  .form {
+    flex: 1;
+  }
+
+  .title {
+    @include text('body-1');
+  }
+
+  .desc {
+    color: var(--waterloo);
+    @include text('body-2');
+  }
+
+  .form {
+    display: flex;
+    position: relative;
+  }
+
+  input {
+    flex: 1;
+    height: 40px;
+    padding: 9px 136px 10px 12px;
+  }
+
+  .submit {
+    position: absolute;
+    top: 4px;
+    right: 4px;
   }
 </style>
