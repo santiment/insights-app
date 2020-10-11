40564fef3a0df5834b563e3dcfcec1c837a54bd3
Nastya Kharitonova <a.kharitonovaspb@gmail.com>
2020-10-11 12:19:02 +0300

    more styles

--- src/components/RocketMoonBtn.svelte
+++ src/components/RocketMoonBtn.svelte
@@ -37,8 +37,11 @@
 include /ui/mixins
 
 button(disabled='{!$currentUser}', on:click='{toggleLike}', aria-label='Like', class='{klass}', class:liked)
-  +icon('rocket').icon
-  span {_likes}
+  .moonWrapper
+    img(src="moon.svg", alt="moon").moon
+    span + 14
+  +icon('rocket').rocket
+  span.text {_likes}
 </template>
 
 <style lang="scss">
@@ -50,6 +53,24 @@ button(disabled='{!$currentUser}', on:click='{toggleLike}', aria-label='Like', c
     background: none;
     border: none;
     outline: none;
+    position: relative;
+    -webkit-tap-highlight-color: transparent;
+    display: inline-flex;
+    align-items: center;
+    line-height: 20px;
+    border: 1px solid var(--porcelain);
+    border-radius: 100px;
+    padding: 4px 8px;
+    color: var(--waterloo);
+    fill: var(--waterloo);
+    transition: color, fill, box-shadow 0.15s ease-in-out;
+
+    &:hover {
+      color: var(--rhino);
+      fill: var(--rhino);
+      border-color: var(--mystic);
+      box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
+    }
 
     &[disabled] {
       pointer-events: none;
@@ -57,8 +78,36 @@ button(disabled='{!$currentUser}', on:click='{toggleLike}', aria-label='Like', c
     }
   }
 
-  .icon {
+  .rocket {
     @include size(20px, 22px);
+    margin: 0 7px 0 0;
+  }
+
+  .moonWrapper {
+    position: absolute;
+    top: -38px;
+    left: -1px;
+    border-radius: 100px;
+    box-shadow: 0 1px 4px 1px rgba(0, 0, 0, 0.1);
+    padding: 8px;
+    display: flex;
+    align-items: center;
+    flex-wrap: nowrap;
+    /*opacity: 0;*/
+    background-color: var(--white);
+    /*transform: translateY(-12px);*/
+    transition: transform 0.4s ease-out, opacity 0.35s ease-out;
+
+    @include text('caption');
+  }
+
+  .moon {
+    margin-right: 4px;
+  }
+
+  .text {
+    text-align: left;
+    @include text('body-3', 'm');
   }
 
 </style>
