5970197ce28d90a885debb152a8d59c240f3ad62
DmitriVanGuard <dimavanguard@gmail.com>
2022-12-29 16:24:17 +0200

    Article NFTBattle side tag

--- src/routes/read/_FixedControls.svelte
+++ src/routes/read/_FixedControls.svelte
@@ -1,8 +1,13 @@
 <script>
+  import Svg from 'webkit/ui/Svg/svelte'
+  import { showChristmasNFTDialog } from 'webkit/ui/ChristmasNFTDialog/Dialog.svelte'
+  import { Page } from 'webkit/ui/ChristmasNFTDialog/types'
+  import { trackNftBattleCTA } from 'webkit/analytics/events/nftbattle'
   import CommentBtn from '@cmp/CommentButton.svelte'
   import ShareBtn from '@cmp/ShareButton.svelte'
   import EditBtn from '@cmp/EditButton.svelte'
   import VoteButton from '@cmp/VoteButton.svelte'
+  import { checkIsNFTTag } from '@/utils/insights'
 
   export let insight
   export let link
@@ -12,7 +17,8 @@
 
   const source = 'insights_article_side_actions'
 
-  $: ({ id, commentsCount } = insight)
+  $: ({ id, commentsCount, tags } = insight)
+  $: isNftBattle = (tags || []).some((tag) => tag && checkIsNFTTag(tag.name))
 </script>
 
 <aside>
@@ -27,6 +33,19 @@
     />
     {#if !isDraft}<ShareBtn {insight} {source} class="mrg-s mrg--b" />{/if}
     {#if isAuthor}<EditBtn {insight} class="" />{/if}
+
+    {#if isNftBattle}
+      <button
+        class="nft btn row v-center"
+        on:click={() => {
+          trackNftBattleCTA(source)
+          showChristmasNFTDialog({ page: Page.Info })
+        }}
+      >
+        <Svg id="nft" w="16" />
+        NFTBattle
+      </button>
+    {/if}
   </div>
 </aside>
 
@@ -47,5 +66,16 @@
   .hidden {
     opacity: 0;
     pointer-events: none;
+  }
+
+  .nft {
+    gap: 8px;
+    padding: 10px 12px;
+    border-radius: 54px;
+    margin: 24px 4px;
+    writing-mode: sideways-lr;
+    --bg: var(--purple-light-1);
+    --color: var(--purple);
+    --color-hover: var(--purple-hover);
   }
 </style>
