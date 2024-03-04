a1b7c1a77ea0af8e5c53eea8ebcf44cc416fe70b
DmitriVanGuard <dimavanguard@gmail.com>
2024-03-04 16:16:11 +0100

    fix: redirect profile route to sanbase

---
+++ src/routes/profile/[slug].svelte
@@ -0,0 +1,7 @@
+<script context="module">
+  export async function preload(page) {
+    const { slug } = page.params
+
+    return this.redirect(302, 'https://app.santiment.net/profile/' + slug)
+  }
+</script>
