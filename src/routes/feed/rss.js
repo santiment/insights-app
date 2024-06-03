916595529f7f2b2185bfc38a2e7935eaec54222b
DmitriVanGuard <dimavanguard@gmail.com>
2024-06-03 20:01:14 +0200

    RSS item pubDate field

--- src/routes/feed/rss.js
+++ src/routes/feed/rss.js
@@ -1,5 +1,6 @@
 import { query } from 'san-webkit/lib/api'
 import { getSEOLinkFromIdAndTitle } from 'san-webkit/lib/utils/url'
+import { getDateFormats } from 'san-webkit/lib/utils/dates'
 import { getRawText } from '../../utils/insights'
 
 function queryInsights() {
@@ -9,6 +10,7 @@ function queryInsights() {
       title
       user {id username}
       text
+      publishedAt
     }
   }
 `)
@@ -26,14 +28,20 @@ function generateDescription(text) {
   return rawText.slice(0, end)
 }
 
+function getPublishDate(publishedAt) {
+  const { DD, MMM, YYYY } = getDateFormats(new Date(publishedAt))
+  return `${DD} ${MMM} ${YYYY}`
+}
+
 export async function get(req, res) {
   const { insights } = await queryInsights()
 
   const rssItems = insights.map(
-    ({ id, title, text }) => `<item>
+    ({ id, title, text, publishedAt }) => `<item>
     <title>${title}</title>
     <link>https://insights.santiment.net/read/${getSEOLinkFromIdAndTitle(id, title)}</link>
     <description>${generateDescription(text)}</description>
+    <pubDate>${getPublishDate(publishedAt)}</pubDate>
   </item>`,
   )
 
@@ -46,6 +54,8 @@ export async function get(req, res) {
   <title>Insights Santiment</title>
   <link>https://insights.santiment.net</link>
   <description>Explore our crypto insights and research, which are based on blockchain and social data. We pioneer using behavioral analytics to enhance your investment decisions.</description>
+  <language>en-US</language>
+  <ttl>1440</ttl>
   ${rssItems.join('\n')}
 </channel>
 
